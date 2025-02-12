fa90d905bdb3ca5d407ed3bfcab513c5a46311b0
Bharat Naik <bharat.anaik2003@gmail.com>
2025-02-12 14:29:18 +0530

    added swarm intelligence

--- js/models.js
+++ js/models.js
@@ -1,108 +1,65 @@
-//#################################
-// longitudinal model
-//#################################
-
-function IDM(v0,T,s0,a,b){
-    this.v0=v0; 
-    this.T=T;
-    this.s0=s0;
-    this.a=a;
-    this.b=b;
-    this.alpha_v0=1; // multiplicator for temporary reduction
-
-    // possible restrictions (value 1000 => initially no restriction)
-    this.speedlimit=1000; // if effective speed limits, speedlimit<v0  
-    this.speedmax=1000; // if vehicle restricts speed, speedmax<speedlimit, v0
-    this.bmax=16;
-    this.calcAcc=function(s,v,vl){
-    //IDM.prototype.calcAcc=function(s,v,vl){ // this works as well but less intuitive
-
-	//if(s<0.1){console.log("IDM.calcAcc: s="+s+" < approx 0!<br>");}
-
-        //MT 2016: noise 
-	var noiseAcc=0.3; // sig_speedFluct=noiseAcc*sqrt(t*dt/12)
-        var accRnd=noiseAcc*(Math.random()-0.5); //if acceleration noise
-
-	var v0eff=Math.min(this.v0, this.speedlimit, this.speedmax);
-	v0eff*=this.alpha_v0;
-       	var accFree=(v<v0eff) ? this.a*(1-Math.pow(v/v0eff,4))
-	    : this.a*(1-v/v0eff);
-	var sstar=this.s0+v*this.T+0.5*v*(v-vl)/Math.sqrt(this.a*this.b);
-	//var sstar=this.s0+v*this.T+0.7*Math.pow(v,0.5)*(v-vl)*Math.pow(Math.abs(v-vl),0.3)/Math.sqrt(this.a*this.b);
-	var accInt=-this.a*Math.pow(sstar/Math.max(s,this.s0),2);
-	var accInt_IDMplus=accInt+this.a;
-	//if(false){
-	//if(this.speedlimit<23){
-	if(this.alpha_v0<0.6){
-          console.log("IDM.calcAcc:"
-	    +" speedlimit="+this.speedlimit
-	    +" speed="+v
-	    +" accFree="+accFree
-	    +" acc="+Math.max(-this.bmax, accFree + accInt + accRnd));
-	}
-	return (v0eff<0.00001) ? 0 : Math.max(-this.bmax, accFree + accInt + accRnd);//IDM
-	//return Math.max(-this.bmax, Math.min(accFree, accInt_IDMplus));//IDMplus
-    }
-}
-
-//#################################
-// lane-changing models
-//#################################
-
-/**
-generalized lane-changing model MOBIL:
-at present no politeness but speed dependent safe deceleration 
-
-@param bSafe:      safe deceleration [m/s^2] at maximum speed v=v0
-@param bSafeMax:   safe deceleration [m/s^2]  at speed zero (gen. higher)
-@param bThr:       lane-changing threshold [m/s^2] 
-@param bBiasRight: bias [m/s^2] to the right
-@return:           MOBIL instance (constructor)
-*/
-
-function MOBIL(bSafe, bSafeMax, bThr, bBiasRight){
-    this.bSafe=bSafe;
-    this.bThr=bThr;
-    this.bBiasRight=bBiasRight;
-    this.bSafeMax=bSafeMax; //!!! transfer into arg list of cstr later on
-    //this.p=p; 
-
-
-
-    /**
-    generalized MOBIL lane chaning decision
-    with bSafe increasing with decrease vrel=v/v0
-    but at present w/o poliuteness
-
-    @param vrel:            v/v0; increase bSave with decreasing vrel
-    @param acc:             own acceleration at old lane
-    @param accNew:          prospective own acceleration at new lane
-    @param accLagTargetNew: prospective accel of new leader
-    @param toRight:         1 if true, 0 if not
-    @return: whether an immediate lane change is safe and desired
-    */
-
-    MOBIL.prototype.realizeLaneChange=function(vrel,acc,accNew,accLagTargetNew,toRight,log){
-
- 	if(log|| (this.bSafe>24)){
-          console.log("\nIn MOBIL.realizeLaneChange");
-	  console.log("  vrel="+vrel
-	              +" acc="+acc+" accNew="+accNew
-		      +" accLagTargetNew="+accLagTargetNew);
-	  console.log("  toRight="+toRight);
-	  console.log("  this.bSafe="+this.bSafe+" this.bThr="+this.bThr
-		      +" this.bBiasRight="+this.bBiasRight);
+class IDM {
+	constructor(v0, T, s0, a, b, noiseAcc = 0.3) {
+		this.v0 = v0;
+		this.T = T;
+		this.s0 = s0;
+		this.a = a;
+		this.b = b;
+		this.alpha_v0 = 1;
+		this.speedlimit = 1000;
+		this.speedmax = 1000;
+		this.bmax = 16;
+		this.noiseAcc = noiseAcc;
 	}
 
-	var bSafeActual=vrel*this.bSafe+(1-vrel)*this.bSafeMax;
+	/**
+	 * Calculates acceleration using IDM logic.
+	 * @param {number} s - Current spacing to the leading vehicle.
+	 * @param {number} v - Current velocity.
+	 * @param {number} vl - Velocity of the leading vehicle.
+	 * @return {number} - Acceleration value.
+	 */
+	calcAcc(s, v, vl) {
+		let accRnd = this.noiseAcc * (Math.random() - 0.5);
+		let v0eff = Math.min(this.v0, this.speedlimit, this.speedmax) * this.alpha_v0;
 
-	if(accLagTargetNew<-bSafeActual){return false;}
-	var dacc=accNew-acc + this.bBiasRight*((toRight) ? 1 : -1)- this.bThr;
+		if (v0eff <= 0) return 0; // Prevents division issues
 
-	if(log || (this.bSafe>24)){
-	  console.log("...dacc="+dacc);
-	  if(dacc>0){console.log("  positive MOBIL LC decision!");}
-	}	
-	return (dacc>0);
-    }
+		let accFree = v < v0eff 
+			? this.a * (1 - Math.pow(v / v0eff, 4)) 
+			: this.a * (1 - v / v0eff);
+
+		let safeDistance = Math.sqrt(this.a * this.b) || 1e-6; // Prevent div by zero
+		let sstar = this.s0 + v * this.T + 0.5 * v * Math.max(v - vl, 0) / safeDistance;
+
+		let accInt = -this.a * Math.pow(sstar / Math.max(s, this.s0), 2);
+		return Math.max(-this.bmax, accFree + accInt + accRnd);
+	}
 }
+
+class MOBIL {
+	constructor(bSafe, bSafeMax, bThr, bBiasRight) {
+		this.bSafe = bSafe;
+		this.bSafeMax = bSafeMax;
+		this.bThr = bThr;
+		this.bBiasRight = bBiasRight;
+	}
+
+	/**
+	 * Determines if a lane change is safe and beneficial.
+	 * @param {number} vrel - Relative speed (v/v0).
+	 * @param {number} acc - Current acceleration in the current lane.
+	 * @param {number} accNew - Expected acceleration in the new lane.
+	 * @param {number} accLagTargetNew - Acceleration of the new lane leader.
+	 * @param {boolean} toRight - Whether the change is to the right lane.
+	 * @return {boolean} - Whether the lane change should occur.
+	 */
+	realizeLaneChange(vrel, acc, accNew, accLagTargetNew, toRight) {
+		let vrelClamped = Math.max(0, Math.min(vrel, 1)); // Clamping for stability
+		let bSafeActual = vrelClamped * this.bSafe + (1 - vrelClamped) * this.bSafeMax;
+		if (accLagTargetNew < -bSafeActual) return false;
+		let laneChangeAdvantage = accNew - acc + this.bBiasRight * (toRight ? 1 : -1) - this.bThr;
+		return laneChangeAdvantage > 0;
+	}
+}
+
